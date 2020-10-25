2e44b01f3b2b90bedb25299e2fed0a885d93ae39
Павел <p-trishkin1@mail.ru>
2020-10-25 19:40:33 +0300

    lesson6

--- src/main/java/ru/gb/trishkin/shop/ShopApplication.java
+++ src/main/java/ru/gb/trishkin/shop/ShopApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.gb.trishkin.shop.config.SellIntegrationConfig;
 
 @SpringBootApplication
 public class ShopApplication {
